7e31271e747a4541765ccde3351e138ea7b1fb88
ramzandashkar <dashkarov@gmail.com>
2023-10-10 11:24:43 +0300

    Создана документация для API

--- users/models.py
+++ users/models.py
@@ -56,5 +56,9 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
         return self.email
