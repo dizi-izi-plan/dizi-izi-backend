1b88ae0a8cfe51d69a4e077a017961517ea100d1
Sergei Morozov <moroz051091@yandex.ru>
2024-02-09 17:59:21 +0400

    Added entrypoint instructions: collectstatic, makemigrations, migrate and starting the gunicorn server

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "----------Collect static files----------"
+python manage.py collectstatic --noinput
+
+echo "----------Make migrations----------"
+python manage.py makemigrations
+
+echo "----------Apply database migrations----------"
+python manage.py migrate
+
+echo "----------Starting Gunicorn----------"
+exec gunicorn config.wsgi:application --bind 0:8000 --workers 3
