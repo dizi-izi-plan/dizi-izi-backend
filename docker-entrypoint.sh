b6fd153fa33b83f853214f1377502f44f7108462
Sergei Morozov <moroz051091@yandex.ru>
2024-02-12 11:10:50 +0400

    chore: add --noinput flag to migrate command in docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo "----------Make migrations----------"
 python manage.py makemigrations
 
 echo "----------Apply database migrations----------"
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "----------Starting Gunicorn----------"
 exec gunicorn config.wsgi:application --bind 0:8000 --workers 3
