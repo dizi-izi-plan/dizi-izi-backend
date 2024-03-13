3559a77a21a8f1435645ac1f738b2acdf8604043
unknown <rspavlov95@gmail.com>
2024-03-14 02:56:31 +0400

    hide server sensetive information

--- config/settings.py
+++ config/settings.py
@@ -113,11 +113,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "dizi.izi.plan@gmail.com"
-DEFAULT_FROM_EMAIL = "dizi.izi.plan@gmail.com"
-EMAIL_HOST_PASSWORD = "tkttxsrnycqeijpw"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
