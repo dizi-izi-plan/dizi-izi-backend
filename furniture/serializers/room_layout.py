3568f57106311c5c1851ce7044af2fb02e7aafbe
Sergei Morozov <moroz051091@yandex.ru>
2024-04-28 22:31:13 +0400

    refactor(furniture.serializers): move business logic to services
    Extract the business logic from the RoomLayoutSerializer in the furniture.serializers module to dedicated service module.

--- furniture/serializers/room_layout.py
+++ furniture/serializers/room_layout.py
@@ -3,14 +3,12 @@ from django.db import transaction
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 
-from furniture.models import (Coordinate, DoorPlacement, Furniture,
-                              FurniturePlacement, PowerSocketPlacement,
-                              RoomLayout, WindowPlacement)
+from furniture.models import Furniture, RoomLayout
 from furniture.serializers import (DoorPlacementSerializer,
                                    FurniturePlacementSerializer,
                                    PowerSocketPlacementSerializer,
                                    WindowPlacementSerializer)
-from layout_algorithm import core
+from furniture.services import create_room_layout
 
 User = get_user_model()
 
@@ -58,120 +56,7 @@ class RoomLayoutSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Создание помещения с расстановкой."""
 
-        def _create_by_coordinate(placement):
-            """Создать и вернуть координаты для элемента (мебель, окно, ...)"""
-            return {
-                "north_west": Coordinate.objects.create(
-                    x=placement["north_west"]["x"],
-                    y=placement["north_west"]["y"],
-                ),
-                "north_east": Coordinate.objects.create(
-                    x=placement["north_east"]["x"],
-                    y=placement["north_east"]["y"],
-                ),
-                "south_west": Coordinate.objects.create(
-                    x=placement["south_west"]["x"],
-                    y=placement["south_west"]["y"],
-                ),
-                "south_east": Coordinate.objects.create(
-                    x=placement["south_east"]["x"],
-                    y=placement["south_east"]["y"],
-                ),
-            }
-
-        room_placement = validated_data.pop("placements")
-        selected_furniture = validated_data.pop("selected_furniture")
-        doors = validated_data.pop("doors")
-        windows = validated_data.pop("windows")
-        power_sockets = validated_data.pop("powersockets")
-        room = RoomLayout.objects.create(**validated_data)
-
-        furniture_placement = []
-        for placement in room_placement:
-            furniture = placement["furniture"]
-            coordinates = _create_by_coordinate(placement)
-            furniture_placement.append(
-                FurniturePlacement(
-                    furniture=furniture,
-                    room=room,
-                    **coordinates,
-                ),
-            )
-        FurniturePlacement.objects.bulk_create(furniture_placement)
-
-        room_doors = []
-        for door in doors:
-            coordinates = _create_by_coordinate(door)
-            room_doors.append(
-                DoorPlacement(
-                    width=door["width"],
-                    open_inside=door["open_inside"],
-                    room=room,
-                    **coordinates,
-                ),
-            )
-        DoorPlacement.objects.bulk_create(room_doors)
-
-        room_windows = []
-        for window in windows:
-            coordinates = _create_by_coordinate(window)
-            room_windows.append(
-                WindowPlacement(
-                    width=window["width"],
-                    length=window["length"],
-                    room=room,
-                    **coordinates,
-                ),
-            )
-        WindowPlacement.objects.bulk_create(room_windows)
-
-        room_powersocket = []
-        for powersocket in power_sockets:
-            coordinates = _create_by_coordinate(powersocket)
-            room_powersocket.append(
-                PowerSocketPlacement(
-                    room=room,
-                    **coordinates,
-                ),
-            )
-        PowerSocketPlacement.objects.bulk_create(room_powersocket)
-
-        furniture_placement = []
-        if selected_furniture:
-            doors_and_windows = []
-            doors_and_windows.extend(doors)
-            doors_and_windows.extend(windows)
-            doors_and_windows.extend(room_placement)
-            furniture = []
-            for one_furniture in selected_furniture:
-                furniture.append(
-                    {
-                        "name": one_furniture.name,
-                        "length": one_furniture.length_access,
-                        "width": one_furniture.width_access,
-                        "power_socket_type": one_furniture.power_socket_type,
-                        "first_power_socket_height": one_furniture.first_power_socket_height,
-                        "first_power_socket_width": one_furniture.first_power_socket_width,
-                        "second_power_socket_height": one_furniture.second_power_socket_height,
-                        "second_power_socket_width": one_furniture.second_power_socket_width,
-                    },
-                )
-            room_size = {
-                "first_wall": room.first_wall,
-                "second_wall": room.second_wall,
-                "third_wall": room.third_wall,
-                "fourth_wall": room.fourth_wall,
-            }
-            furniture_arrangement = core.Core()
-            furniture_arrangement.algorithm_activation(
-                doors_and_windows,
-                furniture,
-                room_size,
-            )
-        for furniture in selected_furniture:
-            # здесь применение алгоритма по расстановке мебели
-            pass
-        return room
+        return create_room_layout(validated_data)
 
 
 class RoomLayoutCopySerializer(serializers.ModelSerializer):
