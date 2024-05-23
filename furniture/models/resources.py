d111f895e5904901532db778eeea8780ee226c8f
StatistIRL <vazsry7@gmail.com>
2024-05-23 23:06:29 +0600

    feat(admin_panel): Add import and export for Furniture and RoomType in admin panel

---
+++ furniture/models/resources.py
@@ -0,0 +1,20 @@
+from import_export import resources
+from . import room_items
+
+
+class FurnitureResource(resources.ModelResource):
+    """Resource класс модели Furniture для работы с import_export библиотекой"""
+
+    class Meta:
+        model = room_items.Furniture
+        import_id_fields = ("name",)
+        skip_unchanged = True
+
+
+class RoomTypeResource(resources.ModelResource):
+    """Resource класс модели RoomType для работы с import_export библиотекой"""
+
+    class Meta:
+        model = room_items.RoomType
+        import_id_fields = ("name",)
+        skip_unchanged = True
