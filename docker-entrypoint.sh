293fd68a9a793cabc5f2dd4e397a7877f2bf9645
chrnmaxim <chrn.maxim@gmail.com>
2024-07-12 14:54:06 +0300

    Change docker-compose to build from local files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 echo "----------Apply database migrations----------"
 python manage.py migrate --noinput
 
+echo "----------Collect backend Static----------"
+python manage.py collectstatic
+
 echo "----------Starting Gunicorn----------"
 exec gunicorn config.wsgi:application --bind 0:8000 --workers 3
