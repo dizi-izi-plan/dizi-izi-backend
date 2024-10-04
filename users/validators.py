4f193f1476ad3b601d963a1dedb86a8f6e4da9d3
Pokatilov-s <semenpokatilov465@gmail.com>
2024-10-04 21:57:22 +0500

    Preparing to extend email validation

--- users/validators.py
+++ users/validators.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from django.core.validators import MaxValueValidator
-
+from django.core.validators import EmailValidator
 
 class PastDateValidator(MaxValueValidator):
     """
@@ -11,3 +11,11 @@ class PastDateValidator(MaxValueValidator):
 
     def __init__(self):
         super().__init__(limit_value=date.today, message=self.message)
+
+
+
+class CustomEmailValidator(EmailValidator):
+    """Extension of standard mail verification"""
+
+    def __call__(self, value):
+        super().__call__(value)
