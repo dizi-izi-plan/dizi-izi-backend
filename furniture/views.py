fcd55b64102e185147f25a2e815db2260bf7a81d
lukonindmitrypy <liondmitry@gmail.com>
2025-02-04 17:46:02 +0300

    fix f8

--- furniture/views.py
+++ furniture/views.py
@@ -11,12 +11,9 @@ from furniture.filters import FurnitureFilter
 from furniture.models import (DoorPlacement, Furniture, FurniturePlacement,
                               PowerSocketPlacement, RoomLayout, RoomType,
                               WindowPlacement)
-from furniture.serializers import (
-    FurnitureSerializer,
-    RoomLayoutListSerializer,
-    RoomLayoutSerializer,
-    RoomTypeSerializer
-)
+from furniture.serializers import (FurnitureSerializer,
+                                   RoomLayoutListSerializer,
+                                   RoomLayoutSerializer, RoomTypeSerializer)
 from furniture.utils import send_pdf_file
 
 
