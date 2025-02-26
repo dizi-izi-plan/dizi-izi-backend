3c48dac3a6337971a282780fddc08b53f18bbbfa
Ksenia909 <kseniaberdugina918@gmail.com>
2025-02-26 14:41:16 +0300

    Feature/postgis room model (#134)
    * Add boundary field (PolygonField) for room geometry
    
    - Added compute_boundary() method to calculate room boundaries.
    - Overridden save() method to automatically fill boundary when saving object.

--- furniture/models/room_layout.py
+++ furniture/models/room_layout.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.gis.db import models as gis_models
+from django.contrib.gis.geos import Polygon
 from django.db import models
 
 from furniture.utils import get_name
@@ -41,6 +43,12 @@ class Room(models.Model):
         help_text='Длина стены в мм',
         validators=(minimum_len_width_validator,),
     )
+    boundary = gis_models.PolygonField(
+        verbose_name='Границы комнаты',
+        srid=0,
+        null=True,
+        blank=True
+    )
 
     class Meta:
         verbose_name = 'Комната'
@@ -48,6 +56,16 @@ class Room(models.Model):
 
     def __str__(self) -> str:
         return f"Комната: {self.name}"
+
+    def compute_boundary(self):
+        width = self.first_wall
+        height = self.second_wall
+        return Polygon(((0, 0), (width, 0), (width, height), (0, height), (0, 0)))
+
+    def save(self, *args, **kwargs):
+        if not self.boundary:
+            self.boundary = self.compute_boundary()
+        super().save(*args, **kwargs)
 
 
 class RoomLayout(models.Model):
