a7c8e4f7c3501165cff807aeef8ff2ba8a5efeb4
Ksenia Berdugina <kseniaberdugina918@gmail.com>
2025-02-27 23:45:49 +0300

    Add model LayoutPlacement
    - Introduced new abstract model LayoutPlacement.
    - Updated FurniturePlacement to inherit from LayoutPlacement.
    - Removed redundant room_layout field from FurniturePlacement.

--- furniture/models/base.py
+++ furniture/models/base.py
@@ -19,3 +19,22 @@ class PlacementCoordinates(gis_models.Model):
 
     class Meta:
         abstract = True
+
+
+class LayoutPlacement(gis_models.Model):
+    """Abstract model with a pointer to a layout and coordinates."""
+
+    room_layout = models.ForeignKey(
+        "RoomLayout",
+        on_delete=models.CASCADE,
+        verbose_name='Планировка',
+        related_name='%(class)ss',
+    )
+    shape = gis_models.PolygonField(
+        verbose_name='Координаты объекта',
+        srid=0,
+        null=True
+    )
+
+    class Meta:
+        abstract = True
