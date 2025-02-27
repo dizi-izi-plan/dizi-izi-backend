06a00880b89cac6ca99de8520587ca05e2f2c719
Ksenia Berdugina <kseniaberdugina918@gmail.com>
2025-02-27 23:08:34 +0300

    Refactoring PlacementCoordinates
    - Removed Coordinate model
    - Replaced RoomLayout model with Room in room field

--- furniture/models/base.py
+++ furniture/models/base.py
@@ -2,25 +2,11 @@ from django.contrib.gis.db import models as gis_models
 from django.db import models
 
 
-class Coordinate(models.Model):
-    """Модель координаты."""
-
-    x = models.PositiveIntegerField(
-        verbose_name='X',
-    )
-    y = models.PositiveIntegerField(
-        verbose_name='Y',
-    )
-
-    def __str__(self):
-        return f'x={self.x}, y={self.y}'
-
-
 class PlacementCoordinates(gis_models.Model):
     """Abstract model with a pointer to a room and coordinates."""
 
     room = models.ForeignKey(
-        "RoomLayout",
+        "Room",
         on_delete=models.CASCADE,
         verbose_name='Комната',
         related_name='%(class)ss',
