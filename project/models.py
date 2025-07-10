19ae28e49b27d7a5d61aa7c54f23277172cd1150
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2025-07-10 20:57:44 +0300

    refactor(project): change literals in model

--- project/models.py
+++ project/models.py
@@ -1,45 +1,46 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
+
+from project.literals import project_literals
 
 User = get_user_model()
 
 
 class Project(models.Model):
     name = models.CharField(
-        verbose_name=settings.PROJECT_NAME_VERBOSE_NAME,
-        max_length=settings.MAX_LENGTH_PROJECT_NAME,
-        default=settings.PROJECT_NAME_BY_DEFAULT,
+        verbose_name=project_literals.project_name_verbose_name,
+        max_length=project_literals.max_length_project_name,
+        default=project_literals.project_name_by_default,
     )
     is_draft = models.BooleanField(
         default=True,
-        verbose_name=settings.PROJECT_DRAFT_VERBOSE_NAME,
+        verbose_name=project_literals.project_draft_verbose_name,
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name=settings.PROJECT_CREATED_AT_VERBOSE_NAME,
+        verbose_name=project_literals.project_created_at_verbose_name,
     )
     updated_at = models.DateTimeField(
         auto_now=True,
-        verbose_name=settings.PROJECT_UPDATED_AT_VERBOSE_NAME,
+        verbose_name=project_literals.project_updated_at_verbose_name,
     )
     layout_requests_left = models.PositiveSmallIntegerField(
-        verbose_name=settings.PROJECT_LAYOUT_REQUESTS_LEFT_VERBOSE_NAME,
-        default=settings.PROJECT_LAYOUT_REQUESTS_LEFT_MAX_VALUE,
+        verbose_name=project_literals.project_layout_requests_left_verbose_name,
+        default=project_literals.project_layout_requests_left_max_value,
         validators=[
             MinValueValidator(
-                settings.PROJECT_LAYOUT_REQUESTS_LEFT_MIN_VALUE,
-                message=settings.PROJECT_LAYOUT_REQUESTS_LEFT_MIN_VALUE_MESSAGE,
+                project_literals.project_layout_requests_left_min_value,
+                message=project_literals.project_layout_requests_left_min_value_message,
             ),
             MaxValueValidator(
-                settings.PROJECT_LAYOUT_REQUESTS_LEFT_MAX_VALUE,
-                message=settings.PROJECT_LAYOUT_REQUESTS_LEFT_MAX_VALUE_MESSAGE,
+                project_literals.project_layout_requests_left_max_value,
+                message=project_literals.project_layout_requests_left_max_value_message,
             ),
         ],
     )
     preferred_layout_id = models.IntegerField(
-        verbose_name=settings.PROJECT_PREFERRED_LAYOUT_VERBOSE_NAME,
+        verbose_name=project_literals.project_preferred_layout_verbose_name,
         null=True,
     )
     user = models.ForeignKey(
@@ -48,10 +49,12 @@ class Project(models.Model):
     )
 
     class Meta:
-        verbose_name = settings.PROJECT_NAME_BY_DEFAULT
-        verbose_name_plural = settings.PROJECTS_VERBOSE_NAME_PLURAL
+        verbose_name = project_literals.project_name_by_default
+        verbose_name_plural = project_literals.projects_verbose_name_plural
 
     def __str__(self) -> str:
-        return (
-            f'Создан проект "{self.name}" для пользователя {self.user} с id {self.pk}'
+        return project_literals.project_str_output.format(
+            self.name,
+            self.user.username,
+            self.pk,
         )
